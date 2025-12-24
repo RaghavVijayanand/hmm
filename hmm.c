2c77f06f3153e6923f8adda759308b9fd29df89e
Raghav Vijayanand <raghav.vijayanand@gmail.com>
2025-12-24 06:56:11 +0530

    Initial commit

--- hmm.c
+++ hmm.c
@@ -4,10 +4,11 @@
 #include <math.h>
 
 #define N 5          // number of states
-#define M 32         // number of symbols (codebook size)
+#define M 32         // codebook size
 #define MAX_T 500
 #define MAX_SEQ 500
-#define DIGITS 4     // 2,3,4,5
+#define DIGITS 4
+#define EPS 1e-6
 
 // ---------------- DATA STRUCTURES ----------------
 
@@ -28,14 +29,23 @@ void init_hmm(HMM *hmm)
 {
     for (int i = 0; i < N; i++)
     {
-        hmm->pi[i] = (i == 0);
+        hmm->pi[i] = (i == 0) ? 1.0 : 0.0;
 
         for (int j = 0; j < N; j++)
-            hmm->A[i][j] = (j == i || j == i + 1) ? 0.5 : 0.0;
+        {
+            if (i == j)
+                hmm->A[i][j] = 0.6;
+            else if (j == i + 1)
+                hmm->A[i][j] = 0.4;
+            else
+                hmm->A[i][j] = 0.0;
+        }
 
         for (int k = 0; k < M; k++)
             hmm->B[i][k] = 1.0 / M;
     }
+
+    hmm->A[N-1][N-1] = 1.0;
 }
 
 // ---------------- LOAD SEQUENCES ----------------
@@ -54,7 +64,7 @@ int load_sequences(const char *file, Sequence *seqs)
         s->T = 0;
 
         char *tok = strtok(line, " \n");
-        while (tok)
+        while (tok && s->T < MAX_T)
         {
             s->O[s->T++] = atoi(tok);
             tok = strtok(NULL, " \n");
@@ -65,62 +75,96 @@ int load_sequences(const char *file, Sequence *seqs)
     return count;
 }
 
-// ---------------- FORWARD ALGORITHM ----------------
+// ---------------- SCALED FORWARD ----------------
 
-double forward(HMM *hmm, Sequence *seq)
+double forward_scaled(HMM *hmm, Sequence *seq)
 {
-    double alpha[MAX_T][N];
-    int T = seq->T;
+    static double alpha[MAX_T][N];
+    static double c[MAX_T];
 
+    int T = seq->T;
+    double logprob = 0;
+
+    c[0] = 0;
     for (int i = 0; i < N; i++)
+    {
         alpha[0][i] = hmm->pi[i] * hmm->B[i][seq->O[0]];
+        c[0] += alpha[0][i];
+    }
+    c[0] = 1.0 / (c[0] + EPS);
+    for (int i = 0; i < N; i++)
+        alpha[0][i] *= c[0];
 
     for (int t = 1; t < T; t++)
+    {
+        c[t] = 0;
         for (int j = 0; j < N; j++)
         {
             alpha[t][j] = 0;
             for (int i = 0; i < N; i++)
                 alpha[t][j] += alpha[t-1][i] * hmm->A[i][j];
             alpha[t][j] *= hmm->B[j][seq->O[t]];
+            c[t] += alpha[t][j];
         }
+        c[t] = 1.0 / (c[t] + EPS);
+        for (int j = 0; j < N; j++)
+            alpha[t][j] *= c[t];
+    }
 
-    double prob = 0;
-    for (int i = 0; i < N; i++)
-        prob += alpha[T-1][i];
+    for (int t = 0; t < T; t++)
+        logprob -= log(c[t]);
 
-    return log(prob + 1e-12);
+    return logprob;
 }
 
 // ---------------- BAUM-WELCH TRAINING ----------------
 
 void baum_welch(HMM *hmm, Sequence *seqs, int S, int iters)
 {
-    static double alpha[MAX_T][N], beta[MAX_T][N];
-    static double gamma[MAX_T][N], xi[MAX_T][N][N];
+    static double alpha[MAX_T][N], beta[MAX_T][N], gamma[MAX_T][N];
+    static double xi[MAX_T][N][N], c[MAX_T];
 
     for (int it = 0; it < iters; it++)
     {
+        double pi_acc[N] = {0};
+        double A_num[N][N] = {{0}}, A_den[N] = {0};
+        double B_num[N][M] = {{0}}, B_den[N] = {0};
+
         for (int s = 0; s < S; s++)
         {
             Sequence *seq = &seqs[s];
             int T = seq->T;
 
-            // Forward
+            // ---- Forward (scaled) ----
+            c[0] = 0;
             for (int i = 0; i < N; i++)
+            {
                 alpha[0][i] = hmm->pi[i] * hmm->B[i][seq->O[0]];
+                c[0] += alpha[0][i];
+            }
+            c[0] = 1.0 / (c[0] + EPS);
+            for (int i = 0; i < N; i++)
+                alpha[0][i] *= c[0];
 
             for (int t = 1; t < T; t++)
+            {
+                c[t] = 0;
                 for (int j = 0; j < N; j++)
                 {
                     alpha[t][j] = 0;
                     for (int i = 0; i < N; i++)
                         alpha[t][j] += alpha[t-1][i] * hmm->A[i][j];
                     alpha[t][j] *= hmm->B[j][seq->O[t]];
+                    c[t] += alpha[t][j];
                 }
+                c[t] = 1.0 / (c[t] + EPS);
+                for (int j = 0; j < N; j++)
+                    alpha[t][j] *= c[t];
+            }
 
-            // Backward
+            // ---- Backward (scaled) ----
             for (int i = 0; i < N; i++)
-                beta[T-1][i] = 1.0;
+                beta[T-1][i] = c[T-1];
 
             for (int t = T-2; t >= 0; t--)
                 for (int i = 0; i < N; i++)
@@ -130,9 +174,10 @@ void baum_welch(HMM *hmm, Sequence *seqs, int S, int iters)
                         beta[t][i] += hmm->A[i][j] *
                                       hmm->B[j][seq->O[t+1]] *
                                       beta[t+1][j];
+                    beta[t][i] *= c[t];
                 }
 
-            // Gamma & Xi
+            // ---- Gamma & Xi ----
             for (int t = 0; t < T-1; t++)
             {
                 double denom = 0;
@@ -149,47 +194,58 @@ void baum_welch(HMM *hmm, Sequence *seqs, int S, int iters)
                         xi[t][i][j] =
                             (alpha[t][i] * hmm->A[i][j] *
                              hmm->B[j][seq->O[t+1]] *
-                             beta[t+1][j]) / (denom + 1e-12);
+                             beta[t+1][j]) / (denom + EPS);
                         gamma[t][i] += xi[t][i][j];
                     }
                 }
             }
 
-            // Update pi
+            // last gamma
+            double denom = 0;
             for (int i = 0; i < N; i++)
-                hmm->pi[i] = gamma[0][i];
+                denom += alpha[T-1][i] * beta[T-1][i];
 
-            // Update A
+            for (int i = 0; i < N; i++)
+                gamma[T-1][i] =
+                    (alpha[T-1][i] * beta[T-1][i]) / (denom + EPS);
+
+            // ---- Accumulate ----
+            for (int i = 0; i < N; i++)
+                pi_acc[i] += gamma[0][i];
+
+            for (int i = 0; i < N; i++)
+                for (int t = 0; t < T-1; t++)
+                    A_den[i] += gamma[t][i];
+
             for (int i = 0; i < N; i++)
                 for (int j = 0; j < N; j++)
-                {
-                    double num = 0, den = 0;
                     for (int t = 0; t < T-1; t++)
-                    {
-                        num += xi[t][i][j];
-                        den += gamma[t][i];
-                    }
-                    hmm->A[i][j] = num / (den + 1e-12);
-                }
+                        A_num[i][j] += xi[t][i][j];
 
-            // Update B
             for (int i = 0; i < N; i++)
-                for (int k = 0; k < M; k++)
+                for (int t = 0; t < T; t++)
                 {
-                    double num = 0, den = 0;
-                    for (int t = 0; t < T; t++)
-                    {
-                        if (seq->O[t] == k)
-                            num += gamma[t][i];
-                        den += gamma[t][i];
-                    }
-                    hmm->B[i][k] = num / (den + 1e-12);
+                    B_den[i] += gamma[t][i];
+                    B_num[i][seq->O[t]] += gamma[t][i];
                 }
         }
+
+        // ---- Re-estimation ----
+        for (int i = 0; i < N; i++)
+            hmm->pi[i] = pi_acc[i] / S;
+
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                hmm->A[i][j] = A_num[i][j] / (A_den[i] + EPS);
+
+        for (int i = 0; i < N; i++)
+            for (int k = 0; k < M; k++)
+                hmm->B[i][k] =
+                    (B_num[i][k] + EPS) / (B_den[i] + M * EPS);
     }
 }
 
-// ---------------- MAIN (TRAIN + TEST + ACCURACY) ----------------
+// ---------------- MAIN ----------------
 
 int main()
 {
@@ -206,7 +262,7 @@ int main()
         int S = load_sequences(path, train);
 
         init_hmm(&models[d]);
-        baum_welch(&models[d], train, S, 15);
+        baum_welch(&models[d], train, S, 25);
 
         printf("Trained HMM for digit %d\n", labels[d]);
     }
@@ -229,7 +285,7 @@ int main()
 
             for (int m = 0; m < DIGITS; m++)
             {
-                double p = forward(&models[m], &test[s]);
+                double p = forward_scaled(&models[m], &test[s]);
                 if (p > best)
                 {
                     best = p;
